a2bee23afe32df7fcf6422dcbe2ec7744c6f20a2
islomovsuz-hue <islomovs.uz@gmail.com>
2026-04-09 20:44:58 +0500

    Admin paneldagi barcha sahifalar Premium darajadagi dizaynga o'tkazildi

--- deploy.bat
+++ deploy.bat
@@ -1,18 +1,17 @@
 @echo off
 echo O'zgarishlar Github'ga yuborilmoqda...
 git add .
-git commit -m "Enhance VPS Management UI to professional design"
+git commit -m "Admin paneldagi barcha sahifalar Premium darajadagi dizaynga o'tkazildi"
 git push origin main
 
+echo.
 echo Server o'zgarishlari amalga oshirilmoqda...
 ssh root@45.138.158.217 "cd /var/www/islomcrm && git pull origin main && systemctl restart islomcrm"
 
+echo.
 echo Keraksiz fayllar o'chirilmoqda...
-del run_git.py
-del run_vps.py
-del .deploy.py
-del run_git2.py
 del deploy.bat
 
-echo Barcha jarayonlar tugadi! Oynani yopishingiz mumkin.
+echo.
+echo Barcha jarayonlar muvaffaqiyatli tugadi! Oynani yopishingiz mumkin.
 pause
